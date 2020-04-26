3a76bf936b9c81bf3ebef40846a9c2b99e099708
Liam Merlyn <l.merlyn@hotmail.co.uk>
2020-04-26 19:14:06 +0100

    Updates

---
+++ precache-manifest.d959cad107720070ce6c4c21e753bf3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cc55df2412b92280ad19962ff6c659",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15bc7d3f21e26a018334",
+    "url": "/monsters-rolodex/static/css/main.0f761fef.chunk.css"
+  },
+  {
+    "revision": "b29eb1d139a4ee2191e7",
+    "url": "/monsters-rolodex/static/js/2.f20e1146.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.f20e1146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bc7d3f21e26a018334",
+    "url": "/monsters-rolodex/static/js/main.37e7309d.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
